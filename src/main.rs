e6076a5e63a96b36b90ed4bceb0606ac2cba5e7f
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-23 05:47:25 +0300

    📝 chore(main.rs): Update argument handling in main function
    The argument handling in the `main` function has been updated to provide a usage message when the required arguments are not provided.
    
    Changes made:
    - Imported the `std::env` module to use the `env::args` function.
    - Added logic to check if the number of arguments is less than 3.
    - If the number of arguments is less than 3, a usage message is printed, indicating the required arguments.
    - The program will exit after displaying the usage message.
    
    This change ensures that the program provides clear instructions to the user when the required arguments are missing.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: {} <search_term> <search_dir>", args[0]);
+        return;
+    }
 }
