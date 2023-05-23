fd1f3701307ab16e6e10fa7864fa8bc99b9fe3ff
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-23 05:53:41 +0300

    ✨ feat(main.rs): Implement Worklist data structure for job management
    A new data structure called `Worklist` has been implemented to manage a list of jobs. This data structure utilizes a shared `Mutex<Vec<Job>>` to ensure thread safety when adding and retrieving jobs from the list.
    
    Changes made:
    - Added a new struct `Worklist` with fields `jobs` of type `Arc<Mutex<Vec<Job>>>`.
    - Implemented the `new()` function to create a new instance of `Worklist` with an empty list of jobs.
    - Implemented the `add()` function to add a job to the `Worklist` by acquiring the lock, pushing the job to the list, and releasing the lock.
    - Implemented the `next()` function to retrieve the next job from the `Worklist` by acquiring the lock, popping a job from the list, and releasing the lock.
    - Updated the `main()` function to include the necessary imports (`env`) and command-line argument check.
    
    This change enables better job management and synchronization in the application.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,28 @@ impl Job {
     }
 }
 
+struct Worklist {
+    jobs: Arc<Mutex<Vec<Job>>>,
+}
+
+impl Worklist {
+    fn new() -> Self {
+        Worklist {
+            jobs: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+
+    fn add(&self, job: Job) {
+        let mut jobs = self.jobs.lock().unwrap();
+        jobs.push(job);
+    }
+
+    fn next(&self) -> Option<Job> {
+        let mut jobs = self.jobs.lock().unwrap();
+        jobs.pop()
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
