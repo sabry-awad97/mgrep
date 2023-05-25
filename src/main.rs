f2b5bcfbbf5c49b1a3a538350f0b04cae0309b55
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-25 12:56:02 +0300

    🎉 feat(Cargo.toml): Add crossbeam-channel dependency ✨ feat(src/main.rs): Introduce crossbeam-channel for thread communication
    The `crossbeam-channel` dependency was added to the `Cargo.toml` file to enable the creation of communication channels between threads. This dependency is required for implementing thread communication in the application.
    
    In `src/main.rs`, the following changes were made:
    - The `crossbeam_channel::unbounded`, `crossbeam_channel::Receiver`, and `crossbeam_channel::Sender` were imported to facilitate the creation of communication channels between threads.
    - The `Worklist` struct was modified to include a `sender` and `receiver` for thread communication.
    - The `add` method was updated to send the job through the communication channel instead of directly modifying the job list.
    - The `next` method was introduced to receive the next job from the communication channel.
    - The `finalize` method was updated to send a special empty job to the communication channel for each worker thread, ensuring they exit gracefully.
    - The `Worker` struct no longer requires the `chunk_size` parameter, as jobs are retrieved one by one from the communication channel.
    - The `process_jobs` method was modified to retrieve jobs one by one from the communication channel and process them accordingly.
    
    Additionally, the unused `search_dir` field in the `Cli` struct was marked as optional.
    
    These changes enable efficient and concurrent processing of jobs by utilizing communication channels between worker threads.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crossbeam_channel::{unbounded, Receiver, Sender}; // Import for creating communication channels between threads
 use rayon::prelude::*; // Import the necessary traits for parallel processing
 use rayon::ThreadPoolBuilder; // Import the ThreadPoolBuilder struct from the rayon crate
 use std::error::Error; // Import the Error trait for error handling
@@ -51,37 +52,32 @@ impl Job {
 
 /// Represents a collection of file search jobs.
 struct Worklist {
-    // A thread-safe list of jobs
-    jobs: Arc<Mutex<Vec<Job>>>,
+    sender: Sender<Option<Job>>,
+    receiver: Receiver<Option<Job>>,
 }
 
 impl Worklist {
     /// Creates a new empty worklist.
     fn new() -> Self {
-        Worklist {
-            jobs: Arc::new(Mutex::new(Vec::new())),
-        }
+        let (sender, receiver) = unbounded();
+        Self { sender, receiver }
     }
 
     /// Adds a new job to the worklist.
     fn add(&self, job: Job) {
-        let mut jobs = self.jobs.lock().unwrap();
-        jobs.push(job);
+        self.sender.send(Some(job)).unwrap();
     }
 
-    /// Retrieves a chunk of jobs from the worklist.
-    fn get_chunk(&self, chunk_size: usize) -> Vec<Job> {
-        let mut jobs = self.jobs.lock().unwrap();
-        let length = jobs.len();
-        let chunk = jobs
-            .splice(0..chunk_size.min(length), vec![]) // Take a chunk of jobs from the list
-            .collect::<Vec<_>>();
-        chunk
+    /// Retrieves the next job from the worklist.
+    fn next(&self) -> Option<Job> {
+        self.receiver.recv().unwrap()
     }
 
-    /// Marks the end of jobs by adding a special empty job to the worklist.
-    fn finalize(&self) {
-        self.add(Job::new(PathBuf::new()));
+    /// Marks the end of jobs by adding a special empty jobs to the worklist.
+    fn finalize(&self, num_workers: usize) {
+        (0..num_workers)
+            .into_par_iter()
+            .for_each(|_| self.sender.send(None).unwrap())
     }
 }
 
@@ -103,23 +99,19 @@ struct Worker {
     worklist: Arc<Worklist>,
     /// A thread-safe list of search results.
     results: Arc<Mutex<Vec<SearchResult>>>,
-    /// The number of jobs to process in a single chunk.
-    chunk_size: usize,
 }
 
 impl Worker {
-    /// Creates a new worker with the specified search term, worklist, results, and chunk size.
+    /// Creates a new worker with the specified search term, worklist, results.
     fn new(
         search_term: String,
         worklist: Arc<Worklist>,
         results: Arc<Mutex<Vec<SearchResult>>>,
-        chunk_size: usize,
     ) -> Self {
         Self {
             search_term,
             worklist,
             results,
-            chunk_size,
         }
     }
 
@@ -160,38 +152,23 @@ impl Worker {
         Ok(matching_lines)
     }
 
-    /// Processes the jobs in chunks until all jobs are completed.
+    /// Processes the jobs until all jobs are completed.
     fn process_jobs(&self) {
         loop {
-            // Get a chunk of jobs to process from the worklist.
-            let jobs = self.worklist.get_chunk(self.chunk_size);
-
-            if jobs.is_empty() {
-                // If there are no jobs left, exit the loop.
+            let job = self.worklist.next();
+            if let Some(job) = job {
+                match self.find_in_file(&job.path) {
+                    Ok(results) => {
+                        let mut result_vec = self.results.lock().unwrap();
+                        result_vec.extend(results);
+                    }
+                    Err(error) => {
+                        eprintln!("Error processing job: {}", error);
+                    }
+                }
+            } else {
                 break;
             }
-
-            let results = jobs
-                // Process jobs in parallel using Rayon, a parallel programming library.
-                .par_iter()
-                // Filter out the `None` values, leaving only successful search results.
-                .filter_map(|job| {
-                    // Call the `find_in_file` function to search for the search term in the file.
-                    // Any I/O errors encountered during the search are logged to the standard error stream.
-                    // The `ok` function transforms the `Result` to an `Option`, discarding the error.
-                    self.find_in_file(&job.path)
-                        .map_err(|error| eprintln!("Error processing job: {}", error))
-                        .ok()
-                })
-                // Flatten the resulting nested iterators into a single iterator of search results.
-                .flatten()
-                .collect::<Vec<_>>(); // Collect the search results into a vector.
-
-            // Obtain a lock on the results vector to ensure exclusive access.
-            let mut result_vec = self.results.lock().unwrap();
-
-            // Extend the results vector with the newly found search results.
-            result_vec.extend(results);
         }
     }
 }
@@ -226,11 +203,10 @@ fn discover_dirs(wl: &Arc<Worklist>, dir_path: &Path) -> Result<(), SearchError>
 #[derive(StructOpt)]
 struct Cli {
     /// The search term
-    #[structopt(required = true)]
     search_term: String,
 
     /// The directory to search in
-    #[structopt(required = true, parse(from_os_str))]
+    #[structopt(parse(from_os_str))]
     search_dir: PathBuf,
 }
 
@@ -242,8 +218,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let search_term = args.search_term.clone();
     let search_dir = args.search_dir;
 
-    // Set the chunk size and determine the number of worker threads
-    let chunk_size = 10;
+    // Determine the number of worker threads
     let num_workers = num_cpus::get();
 
     // Create a shared worklist using `Arc`
@@ -258,7 +233,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         if let Err(error) = discover_dirs(&worklist_clone, &search_dir) {
             eprintln!("Error discovering directories: {}", error);
         }
-        worklist_clone.finalize();
+        worklist_clone.finalize(num_workers);
     });
 
     // Create a thread pool using `ThreadPoolBuilder` to process jobs in parallel
@@ -273,9 +248,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             let search_term_clone = search_term.clone();
 
             // Spawn a thread for each worker and execute the `process_jobs` method
-            s.spawn(|_| {
-                let worker =
-                    Worker::new(search_term_clone, worklist_clone, results_clone, chunk_size);
+            s.spawn(move |_| {
+                let worker = Worker::new(search_term_clone, worklist_clone, results_clone);
                 worker.process_jobs();
             });
         })
